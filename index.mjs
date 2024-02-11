57a3eca6c60ee68edb13c7d891f1d0240fa9a4ae
dasdeo <mark@anio.software>
2024-02-11 23:52:09 +0100

    Swap buggy implementation with an implementation that actually works. Credit: https://stackoverflow.com/users/1048572/bergi.

--- index.mjs
+++ index.mjs
@@ -1,28 +1,41 @@
-export default function createAsyncMutex() {
-	let queue = []
-
-	const releaseNext = () => {
-		if (!queue.length) {
-			return
-		}
-
-		const resolve = queue.shift()
-
-		setTimeout(resolve, 0, () => {
-			releaseNext()
-		})
+//
+// credit: https://stackoverflow.com/a/51086910/2005038
+// by user Bergi https://stackoverflow.com/users/1048572/bergi
+//
+const MutexImplementationFromStackOverflow = class {
+	constructor() {
+		this._lock = null;
 	}
+	isLocked() {
+		return this._lock != null;
+	}
+	_acquire() {
+		var release;
+		const lock = this._lock = new Promise(resolve => {
+			release = resolve;
+		});
+		return () => {
+			if (this._lock == lock) this._lock = null;
+			release();
+		};
+	}
+	acquireSync() {
+		if (this.isLocked()) throw new Error("still locked!");
+		return this._acquire();
+	}
+	acquireQueued() {
+		const q = Promise.resolve(this._lock).then(() => release);
+		const release = this._acquire(); // reserves the lock already, but it doesn't count
+		return q; // as acquired until the caller gets access to `release` through `q`
+	}
+}
+
+export default function createAsyncMutex() {
+	let mutex = new MutexImplementationFromStackOverflow()
 
 	return {
 		acquire() {
-			return new Promise((resolve) => {
-				queue.push(resolve)
-
-				// Resolve immediately if queue was empty
-				if (queue.length === 1) {
-					setTimeout(releaseNext, 0)
-				}
-			})
+			return mutex.acquireQueued()
 		}
 	}
 }
