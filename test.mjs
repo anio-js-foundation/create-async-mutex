95bf6c9daf75f18b65fb46e8f962c4a6423e84c6
dasdeo <mark@anio.software>
2024-02-11 23:51:26 +0100

    Add test file.

---
+++ test.mjs
@@ -0,0 +1,23 @@
+import createAsyncMutex from "./index.mjs"
+
+const mutex = createAsyncMutex()
+
+async function criticalSection(n) {
+	const release = await mutex.acquire()
+	console.log(n, "---- acquired mutex -----")
+
+	await new Promise(r=>setTimeout(r, 1000))
+
+	console.log(n, "---- release mutex ------")
+
+	await release()
+}
+
+criticalSection(1)
+setTimeout(criticalSection, 10, 2)
+
+setTimeout(() => {
+	setTimeout(() => {
+		criticalSection(3)
+	}, 500)
+}, 10)
